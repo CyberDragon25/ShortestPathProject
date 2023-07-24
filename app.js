9fc435e9be9e6c7a87a76830634c5d4eed10506c
Subham <bhattacharya.shubham123@gmail.com>
2023-07-23 21:07:15 -0500

    Deleted the Dikj and PQ, changed code

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
-console.log("Hello World");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("arrayForm");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-const dijkstra = require('./Dikjstra');
+        const rows = parseInt(document.getElementById("rows").value, 10);
+        const columns = parseInt(document.getElementById("columns").value, 10);
 
-// Import the chalk module this is for coloring the array
-// const chalk = require('chalk');
+        if (isNaN(rows) || isNaN(columns) || rows <= 0 || columns <= 0) {
+            alert("Please enter valid positive integers for rows and columns.");
+            return;
+        }
 
-const array2D = new Array(25);
+        const tableContainer = document.getElementById("tableContainer");
+        tableContainer.innerHTML = "";
+        tableContainer.appendChild(create2DArrayTable(rows, columns));
+    });
 
-// Loop to create each row
-for (let i = 0; i < 25; i++) {
-    // Create an array for each row
-    array2D[i] = new Array(25);
-}
+    function create2DArrayTable(rows, columns) {
+        const table = document.createElement("table");
 
-// assinging values to the array
-// setting the value to 0
-for (let i = 0; i < 25; i++) {
-    for (let j = 0; j < 25; j++) {
-        array2D[i][j] = 0;
+        for (let i = 0; i < rows; i++) {
+            const row = document.createElement("tr");
+
+            for (let j = 0; j < columns; j++) {
+                const cell = document.createElement("td");
+                cell.textContent = i + "," + j;
+                row.appendChild(cell);
+            }
+
+            table.appendChild(row);
+        }
+
+        return table;
     }
-
-}
-
-// Print the 2D array as a box
-for (let i = 0; i < 25; i++) {
-    let row = '';
-
-    for (let j = 0; j < 25; j++) {
-        row += array2D[i][j] + ' ';
-    }
-
-    console.log(row);
-}
-
-
-const sourceX = 0;
-const sourceY = 0;
-const destX = 4;
-const destY = 4;
-
-const path = dijkstra(array2D, sourceX, sourceY, destX, destY);
-
-// Print the path as coordinates
-for (const [x, y] of path) {
-    console.log(`(${x}, ${y})`);
-}
+});
